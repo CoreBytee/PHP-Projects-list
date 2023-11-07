4266b0c4170d929db0c590025a02b3808f0a1cc5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-07 15:53:44 +0100

    Create Project.php

---
+++ src/Classes/Project.php
@@ -0,0 +1,54 @@
+<?php
+    include __DIR__ . "/../Helpers/GetConnection.php";
+
+    $_SESSION["ProjectStatements"] = [
+        "GetProjectById" => $DatabaseConnection->prepare("SELECT * FROM projects WHERE Id = :Id"),
+        "GetProjects" => $DatabaseConnection->prepare("SELECT * FROM projects LIMIT :limitcount OFFSET :fromcount")
+    ];
+
+    class Project {
+        public $Id;
+        public $Title;
+        public $Type;
+        public $Year;
+        public $Description;
+        public $Body;
+
+        public function __construct($Data) {
+            $this->Id = $Data["Id"] ?? null;
+            $this->Title = $Data["Title"] ?? null;
+            $this->Type = $Data["Type"] ?? null;
+            $this->Year = $Data["Year"] ?? null;
+            $this->Description = $Data["Description"] ?? null;
+            $this->Body = $Data["Body"] ?? null;
+        }
+
+        public static function GetProjectById($Id) {
+            $Statement = $_SESSION["ProjectStatements"]["GetProjectById"];
+            $Statement->bindParam(":Id", $Id, PDO::PARAM_INT);
+            $Statement->execute();
+
+            $Row = $Statement->fetchAll()[0];
+
+            $Project = new Project($Row);
+            return $Project;
+        }
+
+        public static function GetProjects($Count, $From) {
+            $Statement = $_SESSION["ProjectStatements"]["GetProjects"];
+            $Statement->bindParam(":limitcount", $Count, PDO::PARAM_INT);
+            $Statement->bindParam(":fromcount", $From, PDO::PARAM_INT);
+            $Statement->execute();
+
+            $Rows = array_map(
+                function($Row) {
+                    $Project = new Project($Row);
+                    return $Project;
+                },
+                $_SESSION["ProjectStatements"]["GetProjects"]->fetchAll()
+            );
+
+            return $Rows;
+        }
+    }
+?>
