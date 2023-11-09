94cdd465db6dfad94e1538a65ece3cc313c16270
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-09 13:59:28 +0100

    Update index.php

--- src/login/index.php
+++ src/login/index.php
@@ -1,6 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+    include __DIR__ . "/../Classes/User.php";
+?>
+
+<?php
+
+    function LoginError($ErrorText) {
+        header("Location: /login?error=" . $ErrorText . "&email=" . $_POST["email"], true, 303);
+        exit();
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $User = User::GetUserByEmail($_POST["email"]);
+
+        if (!$User) {
+            LoginError("Invalid email");
+        }
+
+        if ($User && $User->PasswordMatches($_POST["password"], $User->PasswordSalt)) {
+
+            setcookie("sessiontoken", $User->GenerateSession(), time() + 60 * 60 * 24, "/");
+            header("Location: /");
+            exit();
+        } else {
+            LoginError("Invalid password");
+        }
+    }
+
+?>
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -10,17 +40,19 @@
 
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <main class="form-signin w-25 m-auto">
-        <form>
+        <form action="/login" method="post">
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" value="<?= $_GET["email"] ?? "" ?>" />
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password" />
                 <label for="floatingPassword">Password</label>
             </div>
+
+            <a class="text-danger text-decoration-none"><?= $_GET["error"] ?? "" ?></a>
 
             <button class="btn btn-primary w-100 py-2" type="submit">
                 Sign in
